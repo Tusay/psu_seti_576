90fc8ca09d6d42d14fed20d3454994c8088dfb9c
Michael Palumbo <palumbomi95@gmail.com>
2021-03-09 16:39:50 -0500

    going to test pipeline script

--- tSETI_pipeline.py
+++ tSETI_pipeline.py
@@ -1,11 +1,11 @@
 # This code will take a typical set of filterbank data observed in an on/off cadence
 #       and run it through turboSETI and blimpy to produce waterfall plots.
-# 
+#
 # This requires that:
 #       the files be labeled with the standard BL at GBT filename convention.
 #       the observations are ordered in their on/off cadence.
 #       the filterbank files are spliced together.
-#       the folder containing the filterbank files contain only the .fil files 
+#       the folder containing the filterbank files contain only the .fil files
 #               pertaining to the on/off observation and no calibration files.
 #               All other .fil files should be moved to a different folder.
 #
@@ -14,14 +14,14 @@
 #       2. Calculates the maximum drift rate to search for based on barycentric motion
 #               multiplied by an optional input factor.
 #       3. Uses the blimpy package to convert the .fil files to .h5 files.
-#       4. Runs find_doppler in turbo_seti to comb through the h5 files and populate a 
-#               list of hits in a .dat file based on the max drift rate, and a 
+#       4. Runs find_doppler in turbo_seti to comb through the h5 files and populate a
+#               list of hits in a .dat file based on the max drift rate, and a
 #               signal to noise ratio as an optional input, which defaults to 10.
 #       5. Runs find_event_pipeline in turbo_seti to further filter the hits listed in
-#               the .dat files into a .csv file based on another optional signal to 
-#               noise input value that defaults to 10, and a filter_threshold value 
+#               the .dat files into a .csv file based on another optional signal to
+#               noise input value that defaults to 10, and a filter_threshold value
 #               that is explained below.
-#       6. Runs plot_event_pipeline to cross referene the .csv file and the original 
+#       6. Runs plot_event_pipeline to cross referene the .csv file and the original
 #               filterbank data files and produce waterfall plots of any signals.
 #
 # The filter_threshold is meant to serve as automatic RFI rejection and can have one
@@ -36,29 +36,29 @@
 #       requiring a positive detection to appear multiple times. Level 2 is recommended
 #       for new users.
 #
-# This code will output .h5, .dat, and .log files into an output '/processed' folder, 
-#       as well as a list of .fil files used in a fil_files.lst file. It will also 
+# This code will output .h5, .dat, and .log files into an output '/processed' folder,
+#       as well as a list of .fil files used in a fil_files.lst file. It will also
 #       output a list of the .dat files in a dat_files.lst, a .csv file with the refined
 #       table of hits, and any waterfall plots it makes into another subfolder.
 #
 # The input arguments for the code are currently:
-#       '/input_directory/' where all the filterbank .fil files are located, with or 
+#       '/input_directory/' where all the filterbank .fil files are located, with or
 #               without the trailing slash. Note: this is the only mandatory argument.
-#       '-clobber' if present this will purge the output directory of all files in 
-#               order to overwrite them, or skip past any that exist if it is left off. 
-#       '-GPU' utilization is technically possible with turboSETI, though currently 
+#       '-clobber' if present this will purge the output directory of all files in
+#               order to overwrite them, or skip past any that exist if it is left off.
+#       '-GPU' utilization is technically possible with turboSETI, though currently
 #               untested by myself. I intend to test and update this later. Default off.
-#       '-d' this sets the factor by which you want to multiply the max drift rate. 
-#               The default value for this is 1. 
+#       '-d' this sets the factor by which you want to multiply the max drift rate.
+#               The default value for this is 1.
 #       '-s1' this sets the first minimum signal to noise level used by find_doppler.py
 #               when scanning the data for hits. Default value is set to 10.
 #       '-s2' this sets the second signal to noise level used by find_event_pipeline to
 #               further filter the list of hits. Default value is set to 10.
-#       'n' this sets the total number of files in the observation cadence, including 
+#       'n' this sets the total number of files in the observation cadence, including
 #               both on and off files. The maximum value for this is the default at 6.
 #       'f' this sets the filter_threshold as described above. Default is 1.
 #
-# As an example, this code can be run on a pre-curated folder of .fil files from the 
+# As an example, this code can be run on a pre-curated folder of .fil files from the
 #       the terminal window like so:
 # > python nodding_cadence_pipeline.py /input/path/ -clobber -m 10 -s2 20 -n 6 -f 2
 #
@@ -67,6 +67,7 @@
 import numpy as np
 import os
 import sys
+import pdb
 import glob
 import time
 import argparse
@@ -85,7 +86,7 @@ from turbo_seti.find_event.plot_event_pipeline import plot_event_pipeline
 #v The pickle package and function are for code testing purposes only:
 import pickle
 def my_little_pickle(thingy, name):
-    print(f"\nMy little pickle: {name}\n{pickle.loads(pickle.dumps(thingy))}\n") 
+    print(f"\nMy little pickle: {name}\n{pickle.loads(pickle.dumps(thingy))}\n")
 
 #v FUNCTIONS:
 #v This function parses the input arguments:
@@ -129,14 +130,14 @@ def parse_args():
             out += "/"
         if out[0] == "/":
             out = out[1:]
-        odict["out"] = out    
+        odict["out"] = out
     # Returns the input/output arguments as a labeled array
     return odict
 
 
 def get_elapsed_time(start=0):
     end = time.time() - start
-    time_label = 'seconds'    
+    time_label = 'seconds'
     if end > 3600:
         end = end/3600
         time_label = 'hours'
@@ -146,13 +147,13 @@ def get_elapsed_time(start=0):
     return end, time_label
 
 
-#v This function takes in a filename, splits it into an array around the "." character and 
+#v This function takes in a filename, splits it into an array around the "." character and
 #v returns the two digit number in the first slot as a string:
 def get_file_id(filename):
     return str(filename.split(".")[0][-2:])
 
 
-#v This function takes in a filename, splits it into an array around the "_" character and 
+#v This function takes in a filename, splits it into an array around the "_" character and
 #v returns the two digit number in the first slot as a string:
 def get_blc(filename):
     return str(filename.split("_")[0][-2:])
@@ -190,12 +191,12 @@ def sort_by_timestamp(file_list):
 #v This function sorts a list of files by the two digit blc number obtained in the get_blc function above:
 def sort_by_blc(file_list):
     ids = []                                    # Same as the previous function
-    for file in file_list:                      
-        ids.append(get_blc(file))               
-    idx = np.argsort(ids)                       
-    file_list = np.array(file_list)[idx]    
-    file_list = np.ndarray.tolist(file_list)    
-    return file_list                            
+    for file in file_list:
+        ids.append(get_blc(file))
+    idx = np.argsort(ids)
+    file_list = np.array(file_list)[idx]
+    file_list = np.ndarray.tolist(file_list)
+    return file_list
 
 
 def find_first_ON(dat_list, target_name):
@@ -218,9 +219,9 @@ def find_input_data(indir, suffix, zeros_only=True, sort_time=True):
     return file_list                                                #< Returns the list of filterbank .fil files in the input directory to work on
 
 
-def cull_dats(datfile,min_drift):                                                    
+def cull_dats(datfile,min_drift):
     file_array = open(datfile, 'r').read().splitlines()
-    head_num = len(file_array) - 9            
+    head_num = len(file_array) - 9
     header = pd.read_csv(datfile,skipfooter=head_num, engine='python',index_col=False)
     headlist = header.values.tolist()
     df=pd.read_csv(datfile, sep=r'\t', header=[7],skiprows=[8], engine='python')
@@ -250,11 +251,11 @@ def convert_to_h5(file, indir="./", clobber=False):
     out_file = indir + pre + ".h5"                                     #< Create the output filename with the new extension .h5
     if os.path.isfile(out_file):                                        #< Check to see if the output filename already exists
         if not clobber:                                                 #< If the file exists and there is no clobber command then notify
-            print("\n" + pre + ".h5" + " already exists. Moving on...")     
+            print("\n" + pre + ".h5" + " already exists. Moving on...")
             return out_file                                             #< and return the .h5 filename without converting the .fil again
         else:                                                           #< Otherwise...
-            os.remove(out_file)                                         #< If clobber is true, remove the existing output file to overwrite  
-            print("%s has been removed successfully" %out_file) 
+            os.remove(out_file)                                         #< If clobber is true, remove the existing output file to overwrite
+            print("%s has been removed successfully" %out_file)
     print("\nConverting " + pre + " to HDF5...")                           #< Notification that the process will commence
     print("\nPlease ignore the following warning and INFO loggers from blimpy...")
     bp.fil2h5.make_h5_file(file, out_dir=indir)                        #< Pass the .fil file through blimpy to make the .h5 file
@@ -262,13 +263,13 @@ def convert_to_h5(file, indir="./", clobber=False):
     return out_file                                                     #< Return the .h5 output file
 
 
-#v This function runs turboSETI, searching the .h5 input file and populating a .dat file 
+#v This function runs turboSETI, searching the .h5 input file and populating a .dat file
 #v with hits based on the specified max drift rate and signal to noise ratio:
 def run_turbo_seti(file, max_drift=np.nan, min_drift=0, min_snr=10.0, outdir="./", clobber=False, gpu_backend=False):
     assert max_drift != np.nan                                              #< Make sure the drift rate input is a number
     pre, ext = os.path.splitext(os.path.basename(file))                     #< Split the input filename by its file extension
     out_file = outdir + pre + ".dat"                                        #< Create the output filename with the new extension .dat
-    if os.path.isfile(out_file):                                            
+    if os.path.isfile(out_file):
         if not clobber:
             print("\n" + pre + ".dat" + " already exists. Moving on...")
             return out_file
@@ -277,10 +278,10 @@ def run_turbo_seti(file, max_drift=np.nan, min_drift=0, min_snr=10.0, outdir="./
             os.remove(out_file)
             out_log = outdir + pre + ".log"
             os.remove(out_log)
-            print("%s has been removed successfully\n" %out_file) 
+            print("%s has been removed successfully\n" %out_file)
     # call FindDoppler
     log_level_int=logging.WARNING
-    fdop = FindDoppler(datafile=file, max_drift=max_drift, min_drift=min_drift, snr=min_snr, 
+    fdop = FindDoppler(datafile=file, max_drift=max_drift, min_drift=min_drift, snr=min_snr,
                        out_dir=outdir, gpu_backend=gpu_backend, log_level_int=log_level_int)
     # search for hits and report elapsed time.
     startt = time.time()
@@ -290,10 +291,10 @@ def run_turbo_seti(file, max_drift=np.nan, min_drift=0, min_snr=10.0, outdir="./
     print("\n")
     # return the .dat file name
     return out_file
-    
+
 
 def run_find_event_pipeline(datdir, SNR, filter_threshold=np.nan, number_in_cadence=np.nan,
-                                       on_source_complex_cadence=False):
+                            on_source_complex_cadence=False):
     assert filter_threshold != np.nan
     assert number_in_cadence != np.nan
     #get list of files and sort by file ID
@@ -307,23 +308,26 @@ def run_find_event_pipeline(datdir, SNR, filter_threshold=np.nan, number_in_cade
         for item in dat_list:
             f.write("%s\n" % item)
     assert os.path.isfile(lst_dat)
-                # construct csv file name
-                # csv_name = datdir + "hits_SNR_" + SNR + "_f_" + str(filter_threshold) + ".csv"
-                # if os.path.isfile(csv_name):
-                #     os.remove(csv_name)
+
+    # construct csv file name
+    csv_name = datdir + "hits_SNR_" + SNR + "_f_" + str(filter_threshold) + ".csv"
+    if os.path.isfile(csv_name):
+        os.remove(csv_name)
+
     # run find_event_pipeline
     print("\nRunning find event pipeline...")
     find_event_pipeline(lst_dat,
-                        SNR_cut=SNR, 
-                        check_zero_drift=False, 
+                        SNR_cut=SNR,
+                        csv_name=csv_name,
+                        check_zero_drift=False,
                         filter_threshold=filter_threshold,
                         number_in_cadence=number_in_cadence,
                         on_source_complex_cadence=on_source_complex_cadence)
-    try:
-        csv_name = (glob.glob('*f' + str(filter_threshold) + '_snr' + str(SNR) + '*.csv'))[0]
-    except:
-        csv_name = []
-    print("\n")
+    # try:
+    #     csv_name = (glob.glob('*f' + str(filter_threshold) + '_snr' + str(SNR) + '*.csv'))[0]
+    # except:
+    #     csv_name = []
+    # print("\n")
     return csv_name
 
 
@@ -370,7 +374,7 @@ def main():
     # purge any existing fil_files.lst before creating the new one
     if os.path.isfile(lst_fil):
         os.remove(lst_fil)
-        print("%s has been detected and purged to make way for the new list of '.fil' files." %lst_fil) 
+        print("%s has been detected and purged to make way for the new list of '.fil' files." %lst_fil)
     with open(lst_fil, 'w') as f:
         for item in fil_list:
             f.write("%s\n" % item)
@@ -384,48 +388,51 @@ def main():
     for fil in fil_list:
         # convert filterbank file to HDF5
         print(f"\nWorking on file number %s in the list..." %(fil_list.index(fil)+1))
-        outfile = convert_to_h5(fil, indir=indir) 
+        h5_file = convert_to_h5(fil, indir=indir)
+
+        pdb.set_trace()
+
         # Get max drift rate
         print("\nGetting max drift rate based on target coordinates...\n")
-        drift_rate = get_drift_rate(outfile)
+        drift_rate = get_drift_rate(h5_file)
         drifts.append(drift_rate)
         fil_array.append(get_file_id(fil))
         print(f"\nBarycentric drift rate (pre factor) is {drift_rate:.2f}")
         max_drift_rate = drift_rate * max_drift_factor
         min_drift_rate = drift_rate * min_drift_factor                                              # currently broken in find_doppler so doing workaround
-        print(f"Using a drift rate range between {min_drift_rate:.2f} and {max_drift_rate:.2f}.")  
+        print(f"Using a drift rate range between {min_drift_rate:.2f} and {max_drift_rate:.2f}.")
         # move h5 file to output folder temporarily
-        mv_h5_out = 'mv ' + os.path.basename(outfile) + " " + outdir
-        call([mv_h5_out],shell=True)     
+        # mv_h5_out = 'mv ' + os.path.basename(h5_file) + " " + outdir
+        # call([mv_h5_out],shell=True)
         # call FindDoppler
-        datfile = run_turbo_seti(outfile,
+        datfile = run_turbo_seti(h5_file,
                                  min_snr=min_SNR,
                                  outdir=outdir,
                                  clobber=clobber,
                                  max_drift=max_drift_rate,
                                  gpu_backend=gpu_backend)
         # move h5 file back to input folder
-        mv_h5_in = 'mv ' + outdir + os.path.basename(outfile) + " " + indir
-        call([mv_h5_in],shell=True)
+        # mv_h5_in = 'mv ' + outdir + os.path.basename(h5_file) + " " + indir
+        # call([mv_h5_in],shell=True)
         # apply minimum drift rate filter to dat files if indicated
         if min_drift_rate:
             cull_dats(datfile=datfile,min_drift=min_drift_rate)
             print('\nCulling complete.')
         else:
             min_drift_factor=0
-    
+
     max_max_drift_rate=max(drifts) * max_drift_factor
     min_min_drift_rate=min(drifts) * min_drift_factor
     print(f'\nAll hits within drift rate range of {min_min_drift_rate:.4f} and {max_max_drift_rate:.4f} Hz/s.')
-    
-    formatted_drifts = [ '%.4f' % elem for elem in drifts]    
+
+    formatted_drifts = [ '%.4f' % elem for elem in drifts]
     print(f"\nBarycentric drift rates (pre factor) are {formatted_drifts}")
     print(f"\nThese correspond to the files with file ids: {fil_array}")
 
     end_dop, time_label_dop = get_elapsed_time(start_dop)
     print(f"\nTotal time to populate dats with hits using find_doppler: %.2f {time_label_dop}.\n" %end_dop)
-    
-    # remove old dat lists even if clobber off    
+
+    # remove old dat lists even if clobber off
     print("\nRemoving old dat lists...\n")
     old_files = glob.glob(outdir + "dat*.lst")
     if not old_files:
@@ -433,20 +440,19 @@ def main():
     else:
         for file in old_files:
             os.remove(file)
-            print("%s has been removed successfully" %file) 
+            print("%s has been removed successfully" %file)
     # get appropriate list of .dat files in directory
     dat_list = find_input_data(outdir, '.dat')
 
     # call run_find_event_pipeline and make csv file to be placed in the processed folder
-    os.chdir(outdir)               # change into output dir
+    # os.chdir(outdir)               # change into output dir
 
     if target_name:
         number_in_cadence = len(dat_list)
     else:
         target_name=bool(0)
 
-    csv_file = run_find_event_pipeline(datdir=outdir,
-                                       SNR=SNR,
+    csv_file = run_find_event_pipeline(datdir=outdir, SNR=SNR,
                                        filter_threshold=filter_threshold,
                                        number_in_cadence=number_in_cadence,
                                        on_source_complex_cadence=target_name)
@@ -454,15 +460,17 @@ def main():
     # now do the plotting
     # cross-reference the csv file above with the fil_files.lst
     if csv_file:
-        print("\nRunning plot_event_pipeline...")
-        plot_event_pipeline(csv_file, 
-                            lst_fil)
+        # get the directory to save the plot to
         plot_dir = outdir + "f" + str(filter_threshold) + '_s' + str(int(SNR)) + '_plots/'
         if not os.path.isdir(plot_dir):
             os.mkdir(plot_dir)
-        command = 'mv ' + indir + 'f' + str(filter_threshold) + '*.p?? ' + plot_dir  
-        call([command],shell=True)
-        
+
+        print("\nRunning plot_event_pipeline...")
+        plot_event_pipeline(csv_file, lst_fil, plot_dir=plot_dir)
+
+        # command = 'mv ' + indir + 'f' + str(filter_threshold) + '*.p?? ' + plot_dir
+        # call([command],shell=True)
+
     print(f"\nBarycentric drift rates (pre factor) are {formatted_drifts}")
     print(f"\nThese correspond to the files with file ids: {fil_array}")
 
